afd04204ef851ac3997edd2d908157ca1d0b809f
Ana <adehezaflaherty@gmail.com>
2026-05-20 21:34:48 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+# Inicializa la aplicación
+app = Flask(__name__)
+
+# Define la ruta principal (página de inicio)
+@app.route('/')
+def home():
+    username = "Marta"
+    return render_template('index.html', username=username)
+
+# Ejecuta el servidor de desarrollo
+if __name__ == '__main__':
+    app.run(debug=True)
