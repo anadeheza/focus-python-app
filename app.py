efc5f53716d9a68cbfb1d80761a2a75c5e2e4068
Ana <adehezaflaherty@gmail.com>
2026-05-23 13:31:39 -0300

    fix postgres url prefix

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+database_url = os.environ.get('DATABASE_URL', '')
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
